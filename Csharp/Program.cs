fe0c86e5cb46d97e0fd9bc2c6d08ecb962b6418c
Anton <100919590+Aston4ever@users.noreply.github.com>
2024-07-31 17:29:27 +0300

    finish lesson 17

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -1,7 +1,28 @@
 ﻿namespace Csharp;
 
-class Program {
+class Programm {
     static void Main( string[] args ) {
-        Console.WriteLine( "Hello, World!" );
+        int a = 20;
+        int a3 = 10;
+        long a2 = 1000000;
+        float b = 21.2f;
+        double c = 21.34;
+        decimal d = 34.211m;
+        bool f = true;
+        f = false;
+        Console.WriteLine( a + a3 );
+        Console.WriteLine( f );
+        
+        double num1 = 4.5;
+        double num2 = 3.45;
+        double dDiv = num1 / num2;
+        Console.WriteLine( dDiv );
+        
+        float num12 = 4.5f;
+        float num22 = 3.45f;
+        float dDiv2 = num12 / num22;
+        Console.WriteLine( dDiv2 );
+        
+        Console.ReadLine();
     }
 }
--- Csharp/obj/Debug/net8.0/Csharp.AssemblyInfo.cs
+++ Csharp/obj/Debug/net8.0/Csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b6f7e12b715d8c87dcb5f0de6c468aaa4e24089")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a02cb3e8f0e8a449d28116ce928bf7bc23eeda6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
