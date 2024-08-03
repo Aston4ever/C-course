45f8924b0a72c429aa22fff5fa4d418dfc4133cf
Anton <100919590+Aston4ever@users.noreply.github.com>
2024-08-03 13:58:37 +0300

    Methods

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -4,97 +4,130 @@ using static System.Console;
 namespace Csharp;
 
 class Programm { //Class and methods Names PascalNaming
+    const double PI = 3.14159;
+    const int weeks = 52, months = 12;
+    const string DB = "09.05.1989";
+    
     static void Main( string[] args ) {
-        /*
-         int a = 20; // 
-        int a3 = 10;
-        long a2 = 1000000;
-        float b = 21.2f;
-        double c = 21.34;
-        decimal d = 34.211m;
-        bool f = true;
-        f = false;
-        Console.WriteLine( a + a3 );
-        Console.WriteLine( f );
-        double num1 = 4.5;
-        double num2 = 3.45;
-        double dDiv = num1 / num2;
-        Console.WriteLine( dDiv );
-        float num12 = 4.5f;
-        float num22 = 3.45f;
-        float dDiv2 = num12 / num22;
-        Console.WriteLine( dDiv2 );
-        string str = "Aston";
-        str = String.Empty;
-        int strNum = String.GetHashCode( str );
-        str = "MayBe".ToLower();
-        Console.WriteLine( strNum );
-        Console.WriteLine( str );
-        //Finish lesson 20 
-        int ascii = Console.Read();
-        ConsoleColor foreground = Console.ForegroundColor;
-        ConsoleColor background = Console.BackgroundColor;
-        Console.BackgroundColor = ConsoleColor.Red;
-        Console.ForegroundColor = ConsoleColor.Yellow;
-        Console.WriteLine( ascii );
-        Console.BackgroundColor = background;
-        Console.ForegroundColor = foreground;
-        */
-        
-        /*
-         double myDouble = 13.843; 
-        int myInt;
-        //cast double to int
-        myInt = (int)myDouble; //explicit conversion. Отрезает часть, не округляет
-        
-        // implicit conversion
-        int num2 = 1213214;  //Downcast Можно переводить безопасно маленькие числа в большие
-        long num1 = num2;
-        
-        // typeCoversion
-        string myStr = myDouble.ToString();
+     /*
+      int result = AddMethod(3,5);
+     WriteLine(result);
+     WriteLine(AddMethod(5,7));
+     AddMethod( AddMethod( 5, 6 ), AddMethod( 6, 3 ) ); //МЕтод внутри метода, метод как параметр
+     WriteLine(Multiplay(4,7));
+      */
+     /*ConsoleMethod();
+     string? text = ReadLine();
+     ConsoleSpecific(text);*/
+     /*
+      int a = 20; //
+     int a3 = 10;
+     long a2 = 1000000;
+     float b = 21.2f;
+     double c = 21.34;
+     decimal d = 34.211m;
+     bool f = true;
+     f = false;
+     Console.WriteLine( a + a3 );
+     Console.WriteLine( f );
+     double num1 = 4.5;
+     double num2 = 3.45;
+     double dDiv = num1 / num2;
+     Console.WriteLine( dDiv );
+     float num12 = 4.5f;
+     float num22 = 3.45f;
+     float dDiv2 = num12 / num22;
+     Console.WriteLine( dDiv2 );
+     string str = "Aston";
+     str = String.Empty;
+     int strNum = String.GetHashCode( str );
+     str = "MayBe".ToLower();
+     Console.WriteLine( strNum );
+     Console.WriteLine( str );
+     //Finish lesson 20
+     int ascii = Console.Read();
+     ConsoleColor foreground = Console.ForegroundColor;
+     ConsoleColor background = Console.BackgroundColor;
+     Console.BackgroundColor = ConsoleColor.Red;
+     Console.ForegroundColor = ConsoleColor.Yellow;
+     Console.WriteLine( ascii );
+     Console.BackgroundColor = background;
+     Console.ForegroundColor = foreground;
+     */
+     /*
+     double myDouble = 13.843;
+     int myInt;
+     //cast double to int
+     myInt = (int)myDouble; //explicit conversion. Отрезает часть, не округляет
+
+     // implicit conversion
+     int num2 = 1213214;  //Downcast Можно переводить безопасно маленькие числа в большие
+     long num1 = num2;
+
+     // typeCoversion
+     string myStr = myDouble.ToString();
 
 
-        string myStr1 = "15s";
-        string myStr2 = "14";
-        int number1 = int.Parse(myStr1);
-        int number2 = int.Parse(myStr2);
-        int result = number1 + number2;
-        Console.WriteLine( result );
-        */
-        
-        /*
-         int age = 35;
-        string name = "Aston";
-        WriteLine($"Hello, my name is {name} and I am {age}");
-        WriteLine($@"My ///@@$$$@#@!$)
-_(&&)
-_+/n");
-*/
+     string myStr1 = "15s";
+     string myStr2 = "14";
+     int number1 = int.Parse(myStr1);
+     int number2 = int.Parse(myStr2);
+     int resultt = number1 + number2;
+     Console.WriteLine( resultt );
+     */
+     /*
+      int age = 35;
+     string name = "Aston";
+     WriteLine($"Hello, my name is {name} and I am {age}");
+     WriteLine($@"My ///@@$$$@#@!$)
+   _(&&)
+   _+/n");
+   */
+     /*
+      string hello = "       Hello, my name is Aston and i like spaces          ";
+     hello = hello.Trim();
+     string newHello = hello.Substring( 2, 3 );
+     newHello = newHello.ToUpper();
+     */
+     /*
+      Write( "Enter your name: " );
+     string? line = ReadLine();
+     Clear();
+     WriteLine( "--------You names' different variants" );
+     WriteLine( line.ToUpper() );
+     WriteLine(line.ToLower());
+     WriteLine(line.Trim());
+     WriteLine(line.Substring(2,4));
+     ReadKey();
+     Clear();
 
-        /*
-         string hello = "       Hello, my name is Aston and i like spaces          ";
-        hello = hello.Trim();
-        string newHello = hello.Substring( 2, 3 );
-        newHello = newHello.ToUpper();
-        */
-        Write( "Enter your name: " );
-        string? line = ReadLine();
-        Clear();
-        WriteLine( "--------You names' different variants" );
-        WriteLine( line.ToUpper() );
-        WriteLine(line.ToLower());
-        WriteLine(line.Trim());
-        WriteLine(line.Substring(2,4));
-        ReadKey();
-        Clear();
+     WriteLine( "Enter number: " );
+     string? num1 = ReadLine();
 
-        WriteLine( "Enter number: " );
-        string? num1 = ReadLine();
-
-        int num2 = int.Parse(num1);
-        WriteLine(num2);
-        WriteLine( num2.GetTypeCode() );
-        ReadKey();
+     int num2 = int.Parse(num1);
+     WriteLine(num2);
+     WriteLine( num2.GetTypeCode() );
+     ReadKey();
+     */
     }
+    
+    /*
+     static int AddMethod(int x, int y) {
+     return x + y;
+    }
+    static int Multiplay(int x, int y) {
+     return x * y;
+    }
+    */
+    /*
+     static void ConsoleMethod() {
+     WriteLine("Hello from method");
+     ReadKey();
+    }
+
+    static void ConsoleSpecific(string myText) {
+      Clear();
+      WriteLine(myText);
+    }
+    */
 }
--- Csharp/obj/Debug/net8.0/Csharp.AssemblyInfo.cs
+++ Csharp/obj/Debug/net8.0/Csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8d9ba0046157f5316e3c35094a2d4d9151b8f9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94941eb04b53c3849e55a8e8ac6f4c4bd44743d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
