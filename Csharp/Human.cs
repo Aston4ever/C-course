00e8484f416dd23dbd0be83598fb5165588f7988
Anton <100919590+Aston4ever@users.noreply.github.com>
2024-08-06 16:17:27 +0300

    Constructors

--- Csharp/Human.cs
+++ Csharp/Human.cs
@@ -7,7 +7,22 @@ public class Human {
     private string eyeColor;
     private int age;
 
+    public Human() {
+        
+    }
     //cunstructor
+    public Human(string firstName) {
+        this.firstName = firstName;
+    }
+    public Human(string firstName, string lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    public Human(string firstName, string lastName, string eyeColor) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.eyeColor = eyeColor;
+    }
     public Human(string firstName, string lastName, string eyeColor, int age) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -15,6 +30,16 @@ public class Human {
         this.age = age;
     }
     public void IntroduceMyself() {
-        Console.WriteLine($"Hello, {firstName} {lastName}! I have {eyeColor} eyes and i am {age}");
+        if ( age != 0 && firstName != null && lastName != null && eyeColor != null ) {
+            Console.WriteLine($"Hello, {firstName} {lastName}! I have {eyeColor} eyes and i am {age}");
+        } else if (age == 0 && firstName != null && lastName != null && eyeColor != null ){
+            Console.WriteLine($"Hello, {firstName} {lastName}!I have {eyeColor} eyes!");
+        } else if (age == 0 && firstName != null && lastName != null && eyeColor == null) {
+            Console.WriteLine($"Hello, {firstName} {lastName}!");
+        }else if ( age == 0 && firstName != null && lastName == null && eyeColor == null) {
+            Console.WriteLine($"Hello, {firstName}!");
+        } else if ( age == 0 && firstName == null && lastName == null && eyeColor == null) {
+            Console.WriteLine( "Object created, That's all!" );
+        }
     }
 }
--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -8,10 +8,15 @@ class Programm
     static void Main( string[] args ) {
      //create an object
      Human aston = new Human("Aston", "Star", "brown", 12);
-     Human mike = new Human("Mike", "Miller", "Blue", 34);
-     
+     Human mike = new Human("Mike", "Miller", "Blue");
+     Human noOne = new Human();
+     Human Emily = new Human("Emili");
+     Human Melony = new Human("Melony", "Cook");
      aston.IntroduceMyself();
      mike.IntroduceMyself();
+     noOne.IntroduceMyself();
+     Emily.IntroduceMyself();
+     Melony.IntroduceMyself();
      ReadKey();
      
     }
